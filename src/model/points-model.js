9ae26e8b6f04de30c0d50127403e3306c4c9cb14
DenisNilov <denis-nilov@yandex.ru>
2023-08-12 08:45:27 +0300

    7.1 Использует паттерн Наблюдатель

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -1,9 +1,12 @@
-export default class PointsModel {
+import Observable from '../framework/observable.js';
+
+export default class PointsModel extends Observable {
 
   #points = null;
   #service = null;
 
   constructor(service) {
+    super();
     this.#service = service;
     this.#points = this.#service.points;
   }
