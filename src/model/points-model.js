56f9e442945a7f9fb3229e8c8eff6205ee08e578
DenisNilov <denis-nilov@yandex.ru>
2023-09-21 20:52:41 +0300

    8.1.3 Реализует паттерн Адаптер. WIP

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -13,7 +13,7 @@ export default class PointsModel extends Observable {
     this.#pointsApiService = pointsApiService;
 
     this.#pointsApiService.points.then((points) => {
-      console.log(points);
+      console.log(points.map(this.#adaptToClient));
     });
   }
 
@@ -59,5 +59,23 @@ export default class PointsModel extends Observable {
     ];
 
     this._notify(updateType, update);
+  }
+
+  #adaptToClient(point) {
+    const adaptedPoint = {
+      ...point,
+      basePrice: point['base_price'],
+      dateFrom: point['date_from'] !== null ? new Date(point['date_from']) : point['date_from'],
+      dateTo: point['date_to'] !== null ? new Date(point['date_to']) : point['date_to'],
+      isFavorite: point['is_favorite'],
+    };
+
+    // Ненужные ключи мы удаляем
+    delete adaptedPoint['base_price'];
+    delete adaptedPoint['date_from'];
+    delete adaptedPoint['date_to'];
+    delete adaptedPoint['is_favorite'];
+
+    return adaptedPoint;
   }
 }
--- src/points-api-service.js
+++ src/points-api-service.js
@@ -7,7 +7,7 @@ const Method = {
 
 export default class PointsApiService extends ApiService {
   get points() {
-    return this._load({url: 'points'})
+    return this._load({ url: 'points' })
       .then(ApiService.parseResponse);
   }
 
@@ -15,12 +15,31 @@ export default class PointsApiService extends ApiService {
     const response = await this._load({
       url: `points/${point.id}`,
       method: Method.PUT,
-      body: JSON.stringify(point),
-      headers: new Headers({'Content-Type': 'application/json'}),
+      body: JSON.stringify(this.#adaptToServer(point)),
+      headers: new Headers({ 'Content-Type': 'application/json' }),
     });
 
     const parsedResponse = await ApiService.parseResponse(response);
 
     return parsedResponse;
   }
+
+  #adaptToServer(point) {
+    const adaptedPoint = {
+      ...point,
+      'base_price': point.basePrice,
+      'date_from': point.dateFrom instanceof Date ? point.dateFrom.toISOString() : null,
+      'date_to': point.dateTo instanceof Date ? point.dateTo.toISOString() : null,
+      'is_favorite': point.isFavorite
+    };
+
+    // Ненужные ключи мы удаляем
+    delete point.basePrice;
+    delete point.dateFrom;
+    delete point.dateTo;
+    delete point.isFavorite;
+
+    return adaptedPoint;
+  }
+
 }
